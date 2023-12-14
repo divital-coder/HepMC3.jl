609cdd72472748ddac494ae1db8d696b31a7c00a
Graeme A Stewart <graeme.andrew.stewart@cern.ch>
2023-12-14 15:25:39 +0100

    Basic example, work in progress

---
+++ gen/cpp/HepMC3Wrap.cxx
@@ -0,0 +1,4 @@
+// This file is empty for now
+//
+// It can be used to add additional C++ functionality that can then
+// be wrapped into the Julia CxxWrapped library
---
+++ test/basic_tree.jl
@@ -0,0 +1,24 @@
+# Test the wrapped library for a very basic HepMC3 example
+#
+
+# Inline definition of the module, avoid the need for environment setup stuff
+module HepMC3
+
+using CxxWrap
+using Libdl
+
+wrapped_lib_path = joinpath(@__DIR__, "..", "gen", "build", "lib")
+
+include(joinpath(@__DIR__, "..", "gen", "jl", "HepMC3-export.jl"))
+
+@wrapmodule(()->joinpath(wrapped_lib_path, "libHepMC3Wrap.$(Libdl.dlext)"))
+
+function __init__()
+    @initcxx
+end
+
+end
+
+# Import the library
+using .HePMC3
+
