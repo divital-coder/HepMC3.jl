ed46e6d24dd0ea11118a1387a38eb42638dc33d2
Graeme A Stewart <graeme.andrew.stewart@cern.ch>
2024-01-19 12:03:36 +0100

    Enable debug and mask long doubles

--- gen/HepMC3-veto.h
+++ gen/HepMC3-veto.h
@@ -1,3 +1,8 @@
 // Veto file for exports from the HepMC3 headers
 // To find the correct signature for the veto, examine the 
 // CXX files that wrapit produces
+
+// Long double has no mapping in Julia, so we suppress it
+HepMC3::LongDoubleAttribute;
+HepMC3::VectorLongDoubleAttribute;
+
