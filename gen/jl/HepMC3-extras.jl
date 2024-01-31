b3c74280972d12cd7f00914cb460223378a25ffa
Graeme A Stewart <graeme.andrew.stewart@cern.ch>
2024-01-31 10:24:46 +0100

    A few tests of particle object's PID set/get

--- gen/jl/HepMC3-extras.jl
+++ gen/jl/HepMC3-extras.jl
@@ -4,6 +4,6 @@
 # When printing an attribute, convert the datatype to a string
 Base.show(io::IO, a::Main.HepMC3.HepMC3!Attribute) = Base.show(io, value(a))
 
-# Define a more Julia way to update the value of the attribute
+# Define a more Julian way to update the value of the attribute
 value!(a::Main.HepMC3.HepMC3!Attribute, v) = set_value(a, v)
 export value!
--- test/particle.jl
+++ test/particle.jl
@@ -6,5 +6,12 @@ using Test
 include(joinpath(@__DIR__, "load_module.jl"))
 
 @testset "HepMC3 Particle" begin
-    
+    my_particle = HepMC3!GenParticle()
+    @test pid(my_particle) == 0
+    @test pdg_id(my_particle) == 0
+    @test set_pid(my_particle, 11) === nothing
+    @test set_pdg_id(my_particle, 11) === nothing
+    @test pid(my_particle) == 11
+    @test pdg_id(my_particle) == 11
+
 end
